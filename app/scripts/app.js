7be17535b1e32275b275f6ae7815713cac2f716c
dcdrawk <dcdrawk@gmail.com>
2015-01-24 23:56:16 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myappApp
+ * @description
+ * # myappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngMaterial',
+    'ngMdIcons'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .when('/example', {
+        templateUrl: 'views/example.html',
+        controller: 'ExampleCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  })
+  .controller('AppCtrl', function($scope) {
+    $scope.title1 = 'Button';
+    $scope.title4 = 'Warn';
+    $scope.isDisabled = true;
+    $scope.googleUrl = 'http://google.com';
+  });
