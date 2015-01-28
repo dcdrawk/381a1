3739773f2cd1674877ee3c21e3d37ce9bce8e188
dcdrawk <dcdrawk@gmail.com>
2015-01-28 00:30:41 -0800

    added some input forms to character details view/controller, tried to give them a bit of style

--- app/scripts/controllers/details.js
+++ app/scripts/controllers/details.js
@@ -14,5 +14,20 @@ angular.module('myappApp')
       'AngularJS',
       'Karma'
     ];
+    $scope.user = {
+      title: 'Developer',
+      email: 'ipsum@lorem.com',
+      firstName: '',
+      lastName: '' ,
+      company: 'Google' ,
+      address: '1600 Amphitheatre Pkwy' ,
+      city: 'Mountain View' ,
+      state: 'CA' ,
+      biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+      postalCode : '94043'
+    };
+
+    $scope.characterClasses = ['Select a Class', 'Warrior', 'Rogue', 'Mage'];
+    $scope.selectedItem = $scope.characterClasses[0];
 
   });
