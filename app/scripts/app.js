cf39b1012ae47b9b6899049efe9e9339bfe98bee
dcdrawk <dcdrawk@gmail.com>
2015-01-28 16:00:07 -0800

    changed theme colors, added some inputs to the character details view, working on adding variables and cookies next

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,19 +22,19 @@ angular
   ])
   .config(function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
-    .primaryColor('cyan', {
-      'default': '700', // by default use shade 400 from the pink palette for primary intentions
+    .primaryColor('blue-grey', {
+      'default': '700', // by default use shade 700 from the cyan palette for primary intentions
       'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
       'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
       'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
     })
     // If you specify less than all of the keys, it will inherit from the
     // default shades
-    .accentColor('orange', {
-      'default': '600' // use shade 200 for default, and keep all other shades the same
+    .accentColor('amber', {
+      'default': '400' // use shade 600 for default, and keep all other shades the same
     })
     .warnColor('red', {
-      'default': '400' // use shade 200 for default, and keep all other shades the same
+      'default': '600' // use shade 400 for default, and keep all other shades the same
     });
   })
   .config(function ($routeProvider) {
